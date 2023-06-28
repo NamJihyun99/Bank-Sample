b3043667c9274448689353249f79381d0f16f248
NamJihyun99 <namjihyun9936@gmail.com>
2023-06-28 19:20:35 +0900

    dev:> transaction entity

---
+++ src/main/java/shop/mtcoding/bank/domain/transaction/Transaction.java
@@ -0,0 +1,73 @@
+package shop.mtcoding.bank.domain.transaction;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import shop.mtcoding.bank.domain.account.Account;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "transaction_db")
+@Entity
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account withdrawAccount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account depositAccount;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    private Long withdrawAccountBalance;
+    private Long depositAccountBalance;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransactionEnum category;   // WITHDRAW, DEPOSIT, TRANSFER, ALL
+
+    // 계좌가 사라져도 로그는 남아야 함.
+    private String sender;
+    private String receiver;
+    private String tel;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Transaction(Long id, Account withdrawAccount, Account depositAccount,
+                       Long amount, Long withdrawAccountBalance, Long depositAccountBalance,
+                       TransactionEnum category, String sender, String receiver, String tel,
+                       LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.withdrawAccount = withdrawAccount;
+        this.depositAccount = depositAccount;
+        this.amount = amount;
+        this.withdrawAccountBalance = withdrawAccountBalance;
+        this.depositAccountBalance = depositAccountBalance;
+        this.category = category;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.tel = tel;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
