85850a8a6b233c2a96c0c3a4493fbb16f1b11899
NamJihyun99 <namjihyun9936@gmail.com>
2023-07-18 22:36:05 +0900

    dev:>add pattern annotation in userRequestDto

--- src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
+++ src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
@@ -7,18 +7,26 @@ import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserEnum;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 public class UserReqDto {
     @Getter
     @Setter
     public static class JoinReqDto {
 
+        @Pattern(regexp = "", message = "영문/숫자 2~20자 이내로 작성해주세요.")
         @NotEmpty
         private String username;
+
+        @Pattern(regexp = "", message = "영문/숫자 1~20자 이내로 작성해주세요.")
         @NotEmpty
         private String password;
+
+        @Pattern(regexp = "", message = "이메일 형식에 맞게 작성해주세요.")
         @NotEmpty
         private String email;
+
+        @Pattern(regexp = "", message = "영문/한글 1~20자 이내로 작성해주세요.")
         @NotEmpty
         private String fullName;
 
